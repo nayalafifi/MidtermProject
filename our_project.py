f5b8242827d84fa8381ba230c862f3c3007f5095
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-26 15:33:39 +0100

    Update our_project.py

--- our_project.py
+++ our_project.py
@@ -4,3 +4,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+st.header("Now lets move to the analysis!")
+
+# Create a sidebar header and a separator
+st.sidebar.header("Dashboard")
+st.sidebar.markdown("---")
+
+
+df = pd.read_csv('mmh.csv')
+df.head()
+st.markdown("## Columns")
+df.columns
+
+## Description of Dataset
+
+num = st.number_input('No of Rows',5,10)
+st.dataframe(df.head(num))
+
+### Description of the dataset
+
+st.dataframe(df.describe())
+
+if st.button("Show Describe Code"):
+        code = '''df.describe()'''
+        st.code(code, language='python')
+
+if st.button("Generate Report"):
+  import streamlit as st
+  import streamlit.components.v1 as components
+
