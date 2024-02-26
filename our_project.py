e54f347ba490305bc3ee0151fe0679b717cd0d3c
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-26 15:28:43 +0100

    Update our_project.py

--- our_project.py
+++ our_project.py
@@ -1,1 +1,19 @@
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import load_diabetes
+from sklearn.datasets import fetch_california_housing
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics as mt
+import plotly.express as px
+import streamlit as st
+import random
+from PIL import Image
+import altair as alt
+from htbuilder import HtmlElement, div, hr, a, p, img, styles
+from htbuilder.units import percent, px
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
