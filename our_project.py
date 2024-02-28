8ec58125ec1706c9914df2dffce3e13bf2734dfa
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-28 16:29:45 +0100

    Update our_project.py

--- our_project.py
+++ our_project.py
@@ -3,7 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics as mt
+
+
 
 # Create a sidebar header and a separator
 st.sidebar.header("Dashboard")
