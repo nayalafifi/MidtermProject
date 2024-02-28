79ea8e124bb64b689d5c7bc0d097f7311a0034c3
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-28 16:51:38 +0100

    Update our_project.py

--- our_project.py
+++ our_project.py
@@ -92,10 +92,11 @@ y = quantitative_df[selection]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-lin_reg = LinearRegression()
-lin_reg.fit(X_train,y_train)
+# lin_reg = LinearRegression()
+# lin_reg.fit(X_train,y_train)
 
 
-
+coeff_df = pd.DataFrame(lin_reg.coef_, X.columns, columns=['Coefficient'])
+coeff_df
 
 
