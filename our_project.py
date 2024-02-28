142f87071d749bacb2f7de0095ad6b4ccffa6f93
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-28 16:42:03 +0100

    Update our_project.py

--- our_project.py
+++ our_project.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-# from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-# from sklearn import metrics as mt
+from sklearn import metrics as mt
 
 
 
